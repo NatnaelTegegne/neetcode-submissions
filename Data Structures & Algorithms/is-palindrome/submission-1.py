745df6c66303daa6531f1380ec99d857a811c006
Natnael Tegegne <109174442+NatnaelTegegne@users.noreply.github.com>
2026-07-13 11:58:17 -0400

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,21 @@
+import re
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        #s = s.replace(" ", '')
+        s = re.sub(r'[^a-zA-Z0-9]', '', s)
+        s = s.lower()
+
+        left_pointer = 0
+        right_pointer = len(s)-1
+        while(left_pointer < right_pointer):
+            if(s[left_pointer] == " "):
+                left_pointer +=1
+            if(s[right_pointer] == " "):
+                right_pointer -=1
+            if(s[left_pointer] != s[right_pointer]):
+                return False
+            else:
+                left_pointer +=1
+                right_pointer -=1
+        
+        return True
